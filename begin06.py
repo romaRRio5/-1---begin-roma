f17301585bfb72c1481704cd030d5a09f0bd02fd
romaRRio5 <msc.ganadenov.r@gmail.com>
2026-09-21 15:22:21 +0300

    Add files via upload

---
+++ begin06.py
@@ -0,0 +1,6 @@
+a = float(input())
+b = float(input())
+c = float(input())
+V = a*b*c
+S =  2*(a*b+b*c+a*c)
+print (V,S)
---
+++ begin07.py
@@ -0,0 +1,4 @@
+R = float(input())
+L = 2 * 3.14 * R
+S =5 3.14 * R**2
+print (L,S)
